d4dec9ca8adac6941f6f02f06d58bafd72e3f3f3
PEP8 Speaks <pep8speaks@gmail.com>
2019-01-13 02:34:38 +0530

    Fix pep8 errors in gui/elements/_control_bars.py

--- gui/elements/_control_bars.py
+++ gui/elements/_control_bars.py
@@ -2,6 +2,7 @@ from vispy.util.event import EmitterGroup, Event
 
 from .qt import QtControlBars
 
+
 class ControlBars:
     """Controls object.
 
@@ -10,6 +11,7 @@ class ControlBars:
     viewer : Viewer
         Parent viewer.
     """
+
     def __init__(self, viewer):
         self.viewer = viewer
         self.slider_values = (0, 1)
@@ -43,7 +45,8 @@ class ControlBars:
                 msg = '(%.3f, %.3f)' % (cmin, cmax)
                 self.viewer.status = msg
                 self.slider_values = (slidermin, slidermax)
-                self.events.update_slider(values=self.slider_values, enabled=True)
+                self.events.update_slider(
+                    values=self.slider_values, enabled=True)
                 break
         else:
             self.events.update_slider(enabled=False)
